a5b4fe1b872b285d3668e491aebe127392f07241
Matheus Hora <matheus11hora@gmail.com>
2023-08-16 13:12:18 -0300

    comecando

--- lib/src/repositories/barbershop/barbershop_repository.dart
+++ lib/src/repositories/barbershop/barbershop_repository.dart
@@ -1,5 +1,10 @@
 import 'package:barbershop/src/core/fp/either.dart';
+import 'package:barbershop/src/core/exceptions/repository_exception.dart';
+import 'package:barbershop/src/model/barbershop_model.dart';
+import 'package:barbershop/src/model/user_model.dart';
 
 abstract interface class BarbershopRepository {
-  Future<Either>
+  Future<Either<RepositoryException, BarbershopModel>> getMyBarbershop(
+    UserModel userModel,
+  );
 }
---
+++ lib/src/repositories/barbershop/barbershop_repository_impl.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:barbershop/src/core/exceptions/repository_exception.dart';
+import 'package:barbershop/src/core/fp/either.dart';
+import 'package:barbershop/src/core/restClient/rest_client.dart';
+import 'package:barbershop/src/model/barbershop_model.dart';
+import 'package:barbershop/src/model/user_model.dart';
+import 'package:dio/dio.dart';
+
+import 'barbershop_repository.dart';
+
+class BarbershopRepositoryImpl implements BarbershopRepository {
+  final RestClient restClient;
+
+  BarbershopRepositoryImpl({
+    required this.restClient,
+  });
+
+  @override
+  Future<Either<RepositoryException, BarbershopModel>> getMyBarbershop(
+    UserModel userModel,
+  ) async {
+    switch (userModel) {
+      case UserModelADM():
+        final Response(:data) = await restClient.auth.get(
+          '/barbershop',
+          queryParameters: {
+            'user_id': '#userAuthRef' 
+          },
+        );
+      case UserModelEmployee():
+    }
+  }
+}
