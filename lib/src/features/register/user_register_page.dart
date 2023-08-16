80a2e30f8a71e2edcdba7905dda54e17283f1762
Matheus Hora <matheus11hora@gmail.com>
2023-08-16 20:14:08 -0300

    Criando pagina

--- lib/src/features/register/user_register_page.dart
+++ lib/src/features/register/user_register_page.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
 
 class UserRegisterPage extends StatelessWidget {
+  const UserRegisterPage({super.key});
 
-  const UserRegisterPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Criar conta'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Criar conta'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(label: Text('Nome')),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(label: Text('E-mail')),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(label: Text('Senha')),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                decoration:
+                    const InputDecoration(label: Text('Confirmar senha')),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size.fromHeight(56)
+                  
+                ),
+                onPressed: () {},
+                child: Text('Criar conta'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
