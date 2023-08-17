8c040c60c95b8982c72ae9771d76d725668aa983
Matheus Hora <matheus11hora@gmail.com>
2023-08-16 22:33:44 -0300

    Novos

--- lib/src/features/register/user_register_page.dart
+++ lib/src/features/register/user_register_page.dart
@@ -1,10 +1,39 @@
+import 'package:barbershop/src/features/register/user_register_vm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class UserRegisterPage extends StatelessWidget {
+class UserRegisterPage extends ConsumerStatefulWidget {
   const UserRegisterPage({super.key});
 
   @override
+  ConsumerState<UserRegisterPage> createState() => _UserRegisterPageState();
+}
+
+class _UserRegisterPageState extends ConsumerState<UserRegisterPage> {
+  final formKey = GlobalKey<FormState>();
+  final nameEC = TextEditingController();
+  final emailEC = TextEditingController();
+  final passwordC = TextEditingController();
+
+  @override
+  void dispose() {
+    nameEC.dispose();
+    emailEC.dispose();
+    passwordC.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final userRegisterVm = ref.watch(userRegisterVmProvider.notifier);
+    ref.listen(userRegisterVmProvider, (_, state) {
+      switch (state) {
+        case UserRegisterStateStatus.initial:
+        case UserRegisterStateStatus.success:
+        case UserRegisterStateStatus.error:
+      }
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Criar conta'),
@@ -44,9 +73,7 @@ class UserRegisterPage extends StatelessWidget {
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(56)
-
-                ),
+                    minimumSize: const Size.fromHeight(56)),
                 onPressed: () {},
                 child: const Text('Criar conta'),
               )
---
+++ lib/src/features/register/user_register_vm.dart
@@ -0,0 +1,15 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'user_register_vm.g.dart';
+
+enum UserRegisterStateStatus {
+  initial,
+  success,
+  error,
+}
+
+@riverpod
+class UserRegisterVm extends _$UserRegisterVm {
+  @override
+  UserRegisterStateStatus build() => UserRegisterStateStatus.initial;
+}
