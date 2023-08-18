43f7b9745e8c766dee5bc35a96f6f80ae14cf2cb
Matheus Hora <matheus11hora@gmail.com>
2023-08-18 07:39:16 -0300

    fazendo o dto

--- lib/src/features/register/barbershop/barbershop_register_page.dart
+++ lib/src/features/register/barbershop/barbershop_register_page.dart
@@ -1,13 +1,40 @@
+import 'package:barbershop/src/core/ui/helpers/form_helpers.dart';
+import 'package:barbershop/src/features/register/barbershop/barbershop_register_vm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:validatorless/validatorless.dart';
 
+import '../../../core/ui/helpers/messages.dart';
 import '../../../core/ui/widgets/hours_panel.dart';
 import '../../../core/ui/widgets/weekdays_panel.dart';
 
-class BarbershopRegisterPage extends StatelessWidget {
+class BarbershopRegisterPage extends ConsumerStatefulWidget {
   const BarbershopRegisterPage({super.key});
 
   @override
+  ConsumerState<BarbershopRegisterPage> createState() =>
+      _BarbershopRegisterPageState();
+}
+
+class _BarbershopRegisterPageState
+    extends ConsumerState<BarbershopRegisterPage> {
+  final formKey = GlobalKey<FormState>();
+  final nameEC = TextEditingController();
+  final emailEC = TextEditingController();
+
+  @override
+  void dispose() {
+    nameEC.dispose();
+    emailEC.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final barbershopRegisterVm = ref.watch(
+      barbershopRegisterVmProvider.notifier,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cadastrar estabelecimento'),
@@ -15,53 +42,75 @@ class BarbershopRegisterPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 5,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  label: Text('Nome'),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 5,
                 ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  label: Text('E-mail'),
+                TextFormField(
+                  controller: nameEC,
+                  validator: Validatorless.required('Nome obrigatório'),
+                  onTapOutside: (_) => context.unfocus(),
+                  decoration: const InputDecoration(
+                    label: Text('Nome'),
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              WeekdaysPanel(
-                onDayPressed: (value) {
-                  print(value);
-                },
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              HoursPanel(
-                startTime: 6,
-                endTime: 23,
-                onHoursPressed: (int value) {
-                  print(value);
-                },
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(56),
+                const SizedBox(
+                  height: 24,
                 ),
-                onPressed: () {},
-                child: const Text('Cadastrar Estabelecimento'),
-              ),
-            ],
+                TextFormField(
+                  controller: emailEC,
+                  validator: Validatorless.multiple([
+                    Validatorless.required('E-mail é obrigatório'),
+                    Validatorless.email('E-mail inválido')
+                  ]),
+                  onTapOutside: (_) => context.unfocus(),
+                  decoration: const InputDecoration(
+                    label: Text('E-mail'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                WeekdaysPanel(
+                  onDayPressed: (value) {
+                    barbershopRegisterVm.addOrRemoveOpenDay(value);
+                  },
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                HoursPanel(
+                  startTime: 6,
+                  endTime: 23,
+                  onHoursPressed: (int value) {
+                    barbershopRegisterVm.addOrRemoveOpenHour(value);
+                  },
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(56),
+                  ),
+                  onPressed: () {
+                    switch (formKey.currentState?.validate()) {
+                      case false || null:
+                        Messages.showError('Formulário inválido', context);
+                      case true:
+                        barbershopRegisterVm.register(
+                          nameEC.text,
+                          emailEC.text,
+                        );
+                    }
+                  },
+                  child: const Text('Cadastrar Estabelecimento'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/src/features/register/barbershop/barbershop_register_vm.dart
+++ lib/src/features/register/barbershop/barbershop_register_vm.dart
@@ -1,3 +1,4 @@
+import 'package:barbershop/src/core/providers/application_providers.dart';
 import 'package:barbershop/src/features/register/barbershop/barbershop_register_state.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -8,5 +9,39 @@ class BarbershopRegisterVm extends _$BarbershopRegisterVm {
   @override
   BarbershopRegisterState build() => BarbershopRegisterState.initial();
 
-  
+  void addOrRemoveOpenDay(String weekDay) {
+    final openingDays = state.openingDays;
+
+    if (openingDays.contains(weekDay)) {
+      openingDays.remove(weekDay);
+    } else {
+      openingDays.add(weekDay);
+    }
+
+    state = state.copyWith(openingDays: openingDays);
+  }
+
+  void addOrRemoveOpenHour(int hour) {
+    final openingHours = state.openingHours;
+
+    if (openingHours.contains(hour)) {
+      openingHours.remove(hour);
+    } else {
+      openingHours.add(hour);
+    }
+
+    state = state.copyWith(openingHours: openingHours);
+  }
+
+  Future<void> register(String name, String email) async {
+    final repository = ref.watch(barbershopRepositoryProvider);
+
+    final dto = (
+      name: name,
+      email: email,
+      openingDays: '',
+      openingHours: ''
+    );
+    
+  }
 }
