f48549c88785bcf44f193a2f69cbd75668277150
Matheus Hora <matheus11hora@gmail.com>
2023-08-19 13:28:25 -0300

    pagina de cadastrar colaborador

--- lib/src/barbershop_app.dart
+++ lib/src/barbershop_app.dart
@@ -5,6 +5,7 @@ import 'package:barbershop/src/core/ui/widgets/barbershop_loader.dart';
 import 'package:barbershop/src/features/auth/login/login_page.dart';
 import 'package:flutter/material.dart';
 
+import 'features/home/adm/home_adm_page.dart';
 import 'features/register/barbershop/barbershop_register_page.dart';
 import 'features/register/user/user_register_page.dart';
 import 'features/splash/splash_page.dart';
@@ -28,7 +29,7 @@ class BarbershopApp extends StatelessWidget {
             '/auth/login': (_) => const LoginPage(),
             '/auth/register/user': (_) => const UserRegisterPage(),
             '/auth/register/barbershop': (_) => const BarbershopRegisterPage(),
-            '/home/adm': (_) => const Text('adm'),
+            '/home/adm': (_) => const HomeAdmPage(),
             '/home/employee': (_) => const Text('employee'),
           },
         );
---
+++ lib/src/features/employee/register/employee_register_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EmployeeRegisterPage extends StatelessWidget {
+
+  const EmployeeRegisterPage({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('Cadastrar colcaborador'),),
+           body: Container(),
+       );
+  }
+}
