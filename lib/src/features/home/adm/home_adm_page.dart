ba8e01fd8d7e20c4168a93c4abda2d7aaeac18c7
Matheus Hora <matheus11hora@gmail.com>
2023-08-21 23:01:26 -0300

    arrumando bug na home

--- lib/src/features/home/adm/home_adm_page.dart
+++ lib/src/features/home/adm/home_adm_page.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:barbershop/src/core/providers/application_providers.dart';
 import 'package:barbershop/src/core/ui/barbershop_icons.dart';
 import 'package:barbershop/src/core/ui/constants/constants.dart';
 import 'package:barbershop/src/core/ui/widgets/barbershop_loader.dart';
@@ -23,6 +24,7 @@ class HomeAdmPage extends ConsumerWidget {
         backgroundColor: ColorsConstants.brow,
         onPressed: () async {
           await Navigator.of(context).pushNamed('/employee/register');
+          ref.invalidate(getMeProvider);
           ref.invalidate(homeAdmVmProvider);
         },
         child: const CircleAvatar(
