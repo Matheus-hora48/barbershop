fcc6ab1ebe4512cc24eab0ce029aa7c177e86981
Matheus Hora <matheus11hora@gmail.com>
2023-08-25 22:59:46 -0300

    começando a criar a tela

--- lib/src/features/home/employee/home_employee.dart
+++ lib/src/features/home/employee/home_employee.dart
@@ -1,3 +1,6 @@
+import 'package:barbershop/src/core/ui/constants/constants.dart';
+import 'package:barbershop/src/core/ui/widgets/avatar_widget.dart';
+import 'package:barbershop/src/features/home/widgets/home_header.dart';
 import 'package:flutter/material.dart';
 
 class HomeEmployee extends StatelessWidget {
@@ -6,10 +9,78 @@ class HomeEmployee extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-      ),
-      body: Container(),
-    );
+        body: CustomScrollView(
+      slivers: [
+        const SliverToBoxAdapter(
+          child: HomeHeader.withoutFilter(),
+        ),
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              children: [
+                const AvatarWidget(
+                  hideUpload: true,
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                const Text(
+                  'Nome e sobrenome',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 48,
+                ),
+                Container(
+                  width: MediaQuery.sizeOf(context).width * .7,
+                  height: 108,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: ColorsConstants.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'data',
+                        style: TextStyle(
+                          fontSize: 32,
+                          color: ColorsConstants.brow,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        'data',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: ColorsConstants.brow,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 48,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Agendar cliente'),
+                ),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const Text('Ver agenda'),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
